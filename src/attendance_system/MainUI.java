90d5812b6a1c9a4e6a96be3ded22d562dda406fd
root <root@laptop-Latitude-E6420>
2015-12-27 16:04:31 +0800

    create Main UI

--- src/attendance_system/MainUI.java
+++ src/attendance_system/MainUI.java
@@ -38,7 +38,6 @@ public class MainUI {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
 	}
 
 }
