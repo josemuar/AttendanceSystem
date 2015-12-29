560148c39f8ed2026e4463af0545b88621eba9d7
root <root@laptop-Latitude-E6420>
2015-12-29 22:33:51 +0800

    creating register new user form

--- src/attendance_system/MainUI.java
+++ src/attendance_system/MainUI.java
@@ -2,7 +2,15 @@ package attendance_system;
 
 import java.awt.EventQueue;
 
+import javax.swing.JDialog;
 import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MainUI {
 
@@ -38,6 +46,38 @@ public class MainUI {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		JMenu mnUser = new JMenu("User");
+		menuBar.add(mnUser);
+		
+		JMenuItem mntmNewUser = new JMenuItem("New user");
+		mntmNewUser.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new_user_form  form = new new_user_form();
+				form.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+				form.setVisible(true);
+			}
+		});
+		mnUser.add(mntmNewUser);
+		
+		JMenu mnTag = new JMenu("Tag");
+		menuBar.add(mnTag);
+		
+		JMenuItem mntmNewTag = new JMenuItem("New TAG");
+		mnTag.add(mntmNewTag);
+		
+		JMenu mnReport = new JMenu("Report");
+		menuBar.add(mnReport);
+		
+		JMenuItem mntmListUsers = new JMenuItem("List users");
+		mnReport.add(mntmListUsers);
+		
+		JMenuItem mntmListTags = new JMenuItem("list TAGs");
+		mnReport.add(mntmListTags);
 	}
-
+	
 }
